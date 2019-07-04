1404d96dddcc8221fac76aebbec3d3bf7b7c73f0
Mahbub Murshed <murshed@gmail.com>
2019-07-04 00:52:11 -0700

    Intial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const url = require('url');
+const converter = require('./converter')
+
+const server = http.createServer((request, response) => {
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+
+    let bangla = query.bangla;
+    let format = query.format || "bijoy";
+ 
+    let result = converter.ConvertToUnicode(format, bangla);
+    response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+    response.end(result);
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
